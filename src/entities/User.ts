27f07eb8fa7207bcbaac8d92210b724df533e539
Leckie <hsk0094@gmail.com>
2019-12-28 23:44:34 +0900

    Creating the Verification Key

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { IsEmail } from "class-validator";
 import {
   BaseEntity,
--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhote: String\n  createdAt: String!\n  updatedAt: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n}\n\ntype Query {\n  user: User\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
+export const typeDefs = ["type User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Query {\n  user: User\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -15,12 +15,16 @@ export interface User {
   password: string | null;
   phoneNumber: string | null;
   verifiedPhoneNumber: boolean;
-  profilePhote: string | null;
-  createdAt: string;
-  updatedAt: string | null;
+  profilePhoto: string | null;
+  fullName: string | null;
   isDriving: boolean;
   isRiding: boolean;
   isTaken: boolean;
+  lastLng: number | null;
+  lastLat: number | null;
+  lastOrientation: number | null;
+  createdAt: string;
+  updatedAt: string | null;
 }
 
 export interface Verification {
